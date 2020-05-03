537ae77dc75c5db5c033c3d8aa7b2aae9e7c47e2
hulyak <57311842+hulya95@users.noreply.github.com>
2020-05-03 15:57:34 -0700

    create card component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render( <
-  React.StrictMode >
-  <
-  Hello greeting = {
-    "Hello React Ninja"
-  }
-  / > , < /
-  React.StrictMode > ,
-  document.getElementById('root')
+ReactDOM.render(
+	<React.StrictMode>
+		<div>
+			<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+			<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+			<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+		</div>,
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-//progressive web app 
+//progressive web app
