74c901cf819c4bceff080f671a60c383aa801a53
hulyak <57311842+hulya95@users.noreply.github.com>
2020-05-03 17:11:35 -0700

    create app and header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+const App = () => {
+	return (
+		<div>
+			<h1>RoboFriends</h1>
+			<SearchBox />
+			<CardList robots={robots} />;
+		</div>
+	);
+};
+
+export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBox = () => {
+	return <input type="search" placeholder="search robots" />;
+};
+
+export default SearchBox;
