d6b9a309049948f25509b0b4f545c91ed830c286
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-29 22:16:26 -0700

    feat: create error boundry component

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+//if component fails catch the error
+//in production, user sees the error
+class ErrorBoundry extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError : false
+    }
+  }
+//try - catch, change the state if it has error render h1 
+  componentDidCatch(error, info) {
+    this.setState({hasError : true})  
+  }
+
+  render() {
+    if (this.state.hasError) { 
+      return <h1>Oooops. That is not good</h1>
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundry;
