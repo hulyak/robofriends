2c5130d54ac93bd74b84cb4b2e582574647c15b9
hulyak <57311842+hulya95@users.noreply.github.com>
2020-07-29 21:23:33 -0700

    feat: optimize the project

---
+++ src/containers/App.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+// import {robots} from '../robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+  // add state, state describes the app
+  constructor () {
+    super ();
+    this.state = {
+      robots: [],
+      searchfield: '',
+    };
+    console.log (constructor);
+  }
+  //fetch the robots as soon as we run the component
+  componentDidMount () {
+    fetch ('https://jsonplaceholder.typicode.com/users')
+      .then (response => response.json ())
+      .then (users => this.setState ({robots: users}));
+    // console.log(componentDidMount);
+  }
+  onSearchChange = event => {
+    this.setState ({searchfield: event.target.value});
+
+    // function() {
+    // console.log(this.state.robots);
+
+    // this.setState({ robots: filteredRobots });
+  };
+
+  render () {
+    const {robots, searchfield} = this.state;
+    //change robots according to search field
+    const filteredRobots = robots.filter (robot => {
+      return robot.name.toLowerCase ().includes (searchfield.toLowerCase ());
+    });
+    // console.log('render');
+    //if api doesn't return robots quickly add condition
+    // if (!robots.length) {
+    //   return <h1>Loading</h1>;
+    return !robots.length
+      ? <h1>Loading</h1>
+      : <div className="tc">
+          <h1 className="f1">MonsterFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />;
+          </Scroll>
+        </div>;
+  }
+}
+
+export default App;
